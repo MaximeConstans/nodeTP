7666288c0a3a3b3704e78b37dfc2420d2dce649c
maxime-constans <mxconstans@gmail.com>
2022-01-20 08:42:05 +0100

    first commit

---
+++ server/manifest.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('@hapipal/confidence');
+const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $param: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: 'NODE_ENV',
+            $default: {
+                log: ['error', 'start'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: './plugins/swagger',
+            },
+            {
+                plugin: '@hapipal/schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'mysql',
+                            connection: {
+                                host     : process.env.DB_HOST || "localhost",
+                                user     : process.env.DB_USER || "root",
+                                password : process.env.DB_PASSWORD || "root",
+                                database : process.env.DB_DATABASE || "node_tp1"
+                            },
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: 'NODE_ENV',
+                    $default: '@hapipal/hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
